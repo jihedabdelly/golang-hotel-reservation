bed9e06858fe143e5755da660abb91c89f41a869
jihedabdelly <jihednew7@gmail.com>
2024-11-07 18:12:10 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+
+	
+	"github.com/gofiber/fiber/v2"
+)
+
+func main()  {
+	app := fiber.New()
+	app.Get("/foo", handleFoo)
+	app.Listen(":5500")
+}
+
+func handleFoo(c *fiber.Ctx ) error {
+	
+	return c.JSON(map[string]string{"msg": "endpoint reached"})
+}
