ca62b4cb7c158c092ad3ccc7dd47328b3eca1eb4
jihedabdelly <jihednew7@gmail.com>
2024-11-09 23:36:06 +0100

    save port number in a flag string

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"flag"
 
-	
 	"github.com/gofiber/fiber/v2"
 )
 
 func main()  {
+	listenAddress := flag.String("listenAddress", ":5500", "The listen address or port of the API server.")
+	flag.Parse()
 	app := fiber.New()
 
 	apiV1 := app.Group("/api/v1")
@@ -14,11 +16,11 @@ func main()  {
 	app.Get("/foo", handleFoo)
 	apiV1.Get("/user", handleUser)
 	
-	app.Listen(":5500")
+	app.Listen(*listenAddress)
 }
 
 func handleFoo(c *fiber.Ctx ) error {
-	return c.JSON(map[string]string{"msg": "result from /foo endpoint"})
+	return c.JSON(map[string]string{"msg": "result from /foo new endpoint "})
 }
 
 func handleUser(c *fiber.Ctx ) error {
