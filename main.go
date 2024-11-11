c5fe280c2527f10d47a5a73a8dc75b47aeb2ec05
jihedabdelly <jihednew7@gmail.com>
2024-11-11 23:40:40 +0100

    connect to mongodb locally

--- main.go
+++ main.go
@@ -1,13 +1,49 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
 	"golang-hotel-reservation/api"
+	"golang-hotel-reservation/types"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func main()  {
+const dburi = "mongodb://localhost:27017"
+const dbname = "hotel-reservation"
+const userColl = "users"
+
+func main() {
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx := context.Background()
+	coll := client.Database(dbname).Collection(userColl)
+
+	user := types.User{
+		FirstName: "James",
+		LastName: "At the water cooler",
+	}
+	_, err = coll.InsertOne(ctx, user)
+	if err != nil { 
+		log.Fatal(err)
+	}
+
+	var james types.User
+	if err := coll.FindOne(ctx, bson.M{}).Decode(&james); err!= nil {
+		log.Fatal(err)
+	} 
+
+	fmt.Println(james)
+
 	listenAddress := flag.String("listenAddress", ":5500", "The listen address or port of the API server.")
 	flag.Parse()
 
@@ -16,6 +52,6 @@ func main()  {
 
 	apiV1.Get("/user", api.HandleGetUsers)
 	apiV1.Get("/user/:id", api.HandleGetUser)
-	
+
 	app.Listen(*listenAddress)
 }
