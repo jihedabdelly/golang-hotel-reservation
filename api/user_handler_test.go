cbc5b32a186186d5493860b77d525d0f0fcc0048
jihedabdelly <jihednew7@gmail.com>
2024-11-14 00:38:15 +0100

    mock testing the user creation

--- api/user_handler_test.go
+++ api/user_handler_test.go
@@ -1,11 +1,16 @@
 package api
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"golang-hotel-reservation/db"
+	"golang-hotel-reservation/types"
 	"log"
+	"net/http/httptest"
 	"testing"
 
+	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -38,5 +43,38 @@ func setup(t *testing.T) *testdb {
 func TestPostUser(t *testing.T)  {
 	tdb := setup(t)
 	defer tdb.teardown(t)
-	//t.Fail()
+	
+	app := fiber.New()
+	UserHandler := NewUserHandler(tdb.UserStore)
+	app.Post("/", UserHandler.HandlePostUser)
+
+	params := types.CreateUserParams{
+		Email: "some@email.com",
+		FirstName: "cristiano",
+		LastName: "rolando",
+		Password: "sfgzgthzghtzsdgfhb",
+	}
+	b, _ := json.Marshal(params)
+	req := httptest.NewRequest("POST", "/", bytes.NewReader(b))
+	req.Header.Add("Content-Type", "application/json")
+	resp, _ := app.Test(req)
+	var user types.User
+	json.NewDecoder(resp.Body).Decode(&user)
+
+	if len(user.ID) == 0  {
+		t.Errorf("Expecting a user id to be set")
+	}
+	if len(user.EncryptedPassword) > 0  {
+		t.Errorf("Expected the user password not to be included in the json response")
+	}
+	if user.FirstName != params.FirstName {
+		t.Errorf("Expected the firstname to be %s but got %s", params.FirstName, user.FirstName)
+	}
+  if user.LastName != params.LastName {
+		t.Errorf("Expected the lastname to be %s but got %s", params.LastName, user.LastName)
+	}
+	if user.Email != params.Email {
+		t.Errorf("Expected the email to be %s but got %s", params.Email, user.Email)
+	}
+	
 }
