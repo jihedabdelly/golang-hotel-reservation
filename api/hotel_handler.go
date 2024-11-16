85c6a440f4acad6549f1b4c27b243fb5d330e04f
jihedabdelly <jihednew7@gmail.com>
2024-11-16 22:58:58 +0100

    filter rooms based on hotelID

--- api/hotel_handler.go
+++ api/hotel_handler.go
@@ -2,9 +2,10 @@ package api
 
 import (
 	"golang-hotel-reservation/db"
-	"golang-hotel-reservation/types"
 
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type HotelHandler struct {
@@ -30,4 +31,19 @@ func (h *HotelHandler) HandleGetHotels(c *fiber.Ctx) error {
 		return err
 	}
 	return c.JSON(hotels)
+}
+
+func (h *HotelHandler) HandleGetRooms(c *fiber.Ctx) error {
+	id := c.Params("id")
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return err
+	}
+	filter := bson.M{"hotelID": oid}
+	rooms, err := h.roomStore.GetRooms(c.Context(), filter)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(rooms)
 }
--- main.go
+++ main.go
@@ -48,6 +48,7 @@ func main() {
 
 	// hotel handlers
 	apiV1.Get("/hotel", hotelHandler.HandleGetHotels)
+	apiV1.Get("/hotel/:id/rooms", hotelHandler.HandleGetRooms)
 
 	app.Listen(*listenAddress)
 }
