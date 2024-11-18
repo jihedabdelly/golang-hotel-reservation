1546ea29c46692d018a5eedb4c45e652bf3689e9
jihedabdelly <jihednew7@gmail.com>
2024-11-19 00:27:02 +0100

    check validity of email + pass

--- api/auth_handler.go
+++ api/auth_handler.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type AuthHandler struct {
@@ -38,8 +39,11 @@ func (h *AuthHandler) HandleAuthenticate(c *fiber.Ctx) error {
 		return err
 	}
 
+	if err := bcrypt.CompareHashAndPassword([]byte(user.EncryptedPassword), []byte(params.Password)); err != nil {
+		return fmt.Errorf("invalid credentials")
+	}
 
-	fmt.Println(user)
+	fmt.Println("authenticated ->", user)
 
 	return nil
 }
