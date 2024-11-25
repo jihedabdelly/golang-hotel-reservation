12c9f81271cbde34615f433f72288bcc6401eaa6
jihedabdelly <jihednew7@gmail.com>
2024-11-25 10:56:08 +0100

    get booking for only authorized user

--- api/booking_handler.go
+++ api/booking_handler.go
@@ -2,6 +2,8 @@ package api
 
 import (
 	"golang-hotel-reservation/db"
+	"golang-hotel-reservation/types"
+	"net/http"
 
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/bson"
@@ -34,6 +36,15 @@ func (h *BookingHandler) HandleGetBooking(c *fiber.Ctx) error {
 	if err != nil {
 		return err
 	}
-
+	user, ok := c.Context().UserValue("user").(*types.User)
+	if !ok {
+		return err
+	}
+	if booking.UserID != user.ID {
+		return c.Status(http.StatusUnauthorized).JSON(genericResp{
+			Type: "error",
+			Msg: "not authorized",
+		})
+	}
 	return c.JSON(booking)
 }
