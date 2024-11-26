f5f5b2c5f82fb49d3c8920560ad76f910b3f02fd
jihedabdelly <jihednew7@gmail.com>
2024-11-26 12:45:09 +0100

    test helpers

--- api/auth_handler_test.go
+++ api/auth_handler_test.go
@@ -2,10 +2,9 @@ package api
 
 import (
 	"bytes"
-	"context"
 	"encoding/json"
-	"golang-hotel-reservation/db"
-	"golang-hotel-reservation/types"
+	"fmt"
+	"golang-hotel-reservation/db/fixtures"
 	"net/http"
 	"net/http/httptest"
 	"reflect"
@@ -14,29 +13,13 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func insertTestUser(t *testing.T, userStore db.UserStore) *types.User {
-	user, err := types.NewUserFromParams(types.CreateUserParams{
-		Email:     "james@foo.com",
-		FirstName: "james",
-		LastName:  "foo",
-		Password:  "supersecurepassword",
-	})
-	if err != nil {
-		t.Fatal(err)
-	}
-	_, err = userStore.InsertUser(context.TODO(), user)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	return user
-}
-
-func TestAuthenticateWithWrongPasswordFailure(t *testing.T) {
-	tdb := setup()
+func TestAuthenticateWithWrongPassword(t *testing.T) {
+	tdb := setup(t)
 	defer tdb.teardown(t)
+	fixtures.AddUser(tdb.Store, "james", "foo", false)
+
 	app := fiber.New()
-	authHandler := NewAuthHandler(tdb.UserStore)
+	authHandler := NewAuthHandler(tdb.User)
 	app.Post("/auth", authHandler.HandleAuthenticate)
 
 	params := AuthParams{
@@ -53,31 +36,30 @@ func TestAuthenticateWithWrongPasswordFailure(t *testing.T) {
 	if resp.StatusCode != http.StatusBadRequest {
 		t.Fatalf("expected http status of 400 but got %d", resp.StatusCode)
 	}
-
 	var genResp genericResp
 	if err := json.NewDecoder(resp.Body).Decode(&genResp); err != nil {
 		t.Fatal(err)
 	}
-
 	if genResp.Type != "error" {
 		t.Fatalf("expected gen response type to be error but got %s", genResp.Type)
 	}
 	if genResp.Msg != "invalid credentials" {
-		t.Fatalf("expected gen response msg to be invalid credentials but got %s", genResp.Msg)
+		t.Fatalf("expected gen response msg to be <invalid credentials> but got %s", genResp.Msg)
 	}
 }
 
 func TestAuthenticateSuccess(t *testing.T) {
-	tdb := setup()
+	tdb := setup(t)
 	defer tdb.teardown(t)
-	insertedUser := insertTestUser(t, tdb.UserStore)
+	insertedUser := fixtures.AddUser(tdb.Store, "james", "foo", false)
+
 	app := fiber.New()
-	authHandler := NewAuthHandler(tdb.UserStore)
+	authHandler := NewAuthHandler(tdb.User)
 	app.Post("/auth", authHandler.HandleAuthenticate)
 
 	params := AuthParams{
 		Email:    "james@foo.com",
-		Password: "supersecurepassword",
+		Password: "james_foo",
 	}
 	b, _ := json.Marshal(params)
 	req := httptest.NewRequest("POST", "/auth", bytes.NewReader(b))
@@ -94,15 +76,15 @@ func TestAuthenticateSuccess(t *testing.T) {
 	if err := json.NewDecoder(resp.Body).Decode(&authResp); err != nil {
 		t.Fatal(err)
 	}
-
 	if authResp.Token == "" {
-		t.Fatal("expected the JWT token to be present in the auth response")
+		t.Fatalf("expected the JWT token to be present in the auth response")
 	}
-	// Set the encrypted password to an empty string because password isn't returned with json
+	// Set the encrypted password to an empty string, because we do NOT return that in any
+	// JSON response.
 	insertedUser.EncryptedPassword = ""
 	if !reflect.DeepEqual(insertedUser, authResp.User) {
-		//fmt.Println(insertedUser)
-		//fmt.Println(authResp.User)
+		fmt.Println(insertedUser)
+		fmt.Println(authResp.User)
 		t.Fatalf("expected the user to be the inserted user")
 	}
 }
--- api/user_handler_test.go
+++ api/user_handler_test.go
@@ -2,50 +2,24 @@ package api
 
 import (
 	"bytes"
-	"context"
 	"encoding/json"
-	"golang-hotel-reservation/db"
 	"golang-hotel-reservation/types"
-	"log"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/gofiber/fiber/v2"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 
-type testdb struct {
-	db.UserStore
-}
 
-const testdburi = "mongodb://localhost:27017"
 
-func (tbd *testdb) teardown(t *testing.T) {
-	if err := tbd.UserStore.Drop(context.TODO()); err != nil {
-		t.Fatal(err)
-	}
-}
-
-func setup() *testdb {
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(testdburi))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return &testdb{
-		UserStore: db.NewMongoUserStore(client),
-	}
-
-}
 
 func TestPostUser(t *testing.T)  {
-	tdb := setup()
+	tdb := setup(t)
 	defer tdb.teardown(t)
 	
 	app := fiber.New()
-	UserHandler := NewUserHandler(tdb.UserStore)
+	UserHandler := NewUserHandler(tdb.User)
 	app.Post("/", UserHandler.HandlePostUser)
 
 	params := types.CreateUserParams{
